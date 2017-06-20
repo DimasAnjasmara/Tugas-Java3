c2c4a03b6b2c19f8542957a70340791ad67ba352
dimas anjasmara <dimas.anjasmara@gmail.com>
2017-06-20 15:16:17 +0700

    Tugas Java 3

---
+++ src/tugasjava3baru/TugasJava3Baru.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasjava3baru;
+
+/**
+ *
+ * @author INDOPORCELAIN
+ */
+public class TugasJava3Baru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new TugasJava3().setVisible(true);
+    }
+}
